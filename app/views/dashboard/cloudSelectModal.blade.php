3ce8863d422f06d8e462c33e3fd53bf218fce6ba
abhishek nair <abhishek.alchemist@gmail.com>
2014-01-17 17:36:41 +0530

    new modals and folders in temp

---
+++ app/views/dashboard/cloudSelectModal.blade.php
@@ -0,0 +1,13 @@
+<div class="modal fade select-cloud" id="SelectModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title">Select Clouds</h4>
+      </div>
+      <div class="modal-body">
+        <a href="{{ route('authenticate_route') }}"><img src={{ asset('packages/img/dropbox-icon.png') }}></a>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
