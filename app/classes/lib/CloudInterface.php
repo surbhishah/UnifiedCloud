553f81aafdb93255ae386f0090cdf990a3f27ad5
abhishek nair <abhishek.alchemist@gmail.com>
2014-01-17 17:29:16 +0530

    updated CLoudInterface

--- app/classes/lib/CloudInterface.php
+++ app/classes/lib/CloudInterface.php
@@ -3,4 +3,14 @@ interface CloudInterface{
 	public function upload($userID, $userfile, $cloudDestinationPath);
 	public function download($userID, $cloudSourcePath,$fileName);
 
+	public function getRegistrationPage();
+	public function getCompletion();
+	public function getFullFileStructure($userID);
+	public function refreshFullFileStructure($userID);
+	public function delete($userID,$path);
+	public function createFolder($userID,$folderPath);
+	public function getFolderContents($userID, $folderPath);
+	
+
+
 }
