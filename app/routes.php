d50b33b964193771b3c6dff958994bb865fd309d
abhishek nair <abhishek.alchemist@gmail.com>
2014-01-20 09:27:47 +0530

    tidy routes

--- app/routes.php
+++ app/routes.php
@@ -11,51 +11,51 @@
 |
 */
 
-/*
-Route::get('/', function()
-{
-	return View::make('hello');
-});
-
-/* test routes*/
-
-
-Route::get('/test/master', function()
+// open front page 
+Route::get('/', array( 'as' => 'landing', function()
 {
 	return View::make('landing.landing');
-});
+}));
 
-Route::get('/test/front', function()
-{
-	return View::make('landing.front');
-});
+//user login
+Route::post('user/login',array(
+	'as' => 'sign_in' , 
+	'uses' => 'UsersController@postSignin'
+	));
 
-Route::get('/test/dashboard', function()
-{
-	return View::make('dashboard.dashboard');
-});
+//user signup
+Route::post('user/signup',array(
+	'as' => 'sign_up',
+	'uses' => 'UsersController@postCreate'
+	));
 
-// Route::get('/login',function(){
-//     return View::make('user.login');
-// });
 
-Route::post('user/login',array('as' => 'sign_in' , 'uses' => 'UsersController@postSignin'));
+Route::get('user/register',array(
+	'as' => 'register', 
+	'uses' => 'UsersController@getRegister'
+	));
 
-Route::get('user/login',function() {
-    return View::make('landing.landing');
-});
 
-Route::get('user/logout', array('as' => 'logout' , 'uses' => 'UsersController@getLogout'));
+//user signin 
+Route::get('signin', array( 'as' => 'sign_in_page',function() {
+    return View::make('user.signin');
+}));
 
-// Route::get('/users/dashboard',function() {
-//     return View::make('dashboard.dashboard');
-// });
+//user signup
+Route::get('signup', array('as' => 'sign_up_page',function() {
+    return View::make('user.signup');
+}));
 
-Route::get('user/dashboard', array('as' => 'dashboard', 'uses' => 'UsersController@getDashboard'));
-Route::post('signup',array('as' => 'sign_up','uses' => 'UsersController@postCreate'));
+//logout
+Route::get('user/logout', array(
+	'as' => 'logout' , 
+	'uses' => 'UsersController@getLogout'
+	));
 
-Route::get('user/register',array('as' => 'register', 'uses' => 'UsersController@getRegister'));
-
+Route::get('user/dashboard', array(
+	'as' => 'dashboard', 
+	'uses' => 'UsersController@getDashboard'
+	));
 
 
 // UNCOMMENT these two routes when you need authentication of dropbox 
@@ -65,60 +65,61 @@ Route::get('user/register',array('as' => 'register', 'uses' => 'UsersController@
 
 //Route::get('user/{cloud}',array('as'=>'authenticate', 'uses'=>'UsersController@getRegistrationPage'));
 
+// Route to user authentication
 Route::get('authenticate',array(
 	'as'=>'authenticate_route',
 	'uses'=>'UsersController@getRegistrationPage'
 	));
 
-
+//authorization from dropbox
 Route::get('auth/dropbox',array(
 	'as'=>'completion_route',
 	'uses'=>'UsersController@getCompletion'
 	));
-											// home page
+
+// home page
 Route::get('user/home',array(
 	'as'=>'home_route',
 	'uses'=>'UsersController@getHome'
 ));
 
-											// download
-/*Route::get('user/download/{cloudName}/{cloudSourcePath}/{fileName}',array(
-	'as'=>'download_route',
-	'uses'=>'FilesController@getFile'
-));*/
+// download
 Route::get('user/download',array(
 	'as'=>'download_route',
 	'uses'=>'FilesController@getFile'
 ));
-											//upload
+
+//upload
 Route::post('user/upload/{cloudName}',array(
 	'as'=>'upload_route',
 	'uses'=>'FilesController@postFile'
 ));
 
-											// get folder content
+// get folder content
 Route::get('user/folder_content',array(
 	'as'=>'folder_content_route',
 	'uses'=>'FilesController@getFolderContents'
 ));
 
-												// create folder 
+// create folder 
 Route::get('user/new_folder/{cloudName}/{folderPath}',array(
 	'as'=>'create_folder_route',
 	'uses'=>'FilesController@getCreateFolder'
 ));
 
-												// delete a file or folder 
+// delete a file or folder 
 Route::delete('user/delete/{cloudName}/{folderPath}',array(
 	'as'=>'delete_route',
 	'uses'=>'FilesController@delete'
 ));
-												//add a new cloud 
+
+//add a new cloud 
 Route::get('user/add_cloud/{cloudName}',array(
 	'as'=>'add_cloud_route',
 	'uses'=>'FilesController@getAddCloud'
 ));
-												//refresh cloud
+
+//refresh cloud
 Route::get('user/refresh/{cloudName}',array(
 	'as'=>'refresh_route',
 	'uses'=>'FilesController@getRefresh'
