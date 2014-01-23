a16355e52e240264aa921dec31cb5af6f59d643b
Surbhi <surbhishah81@yahoo.in>
2014-01-23 23:17:15 +0530

    Modified the database...email_cloud to user_cloud_name and also the unique constraint

--- app/database/migrations/2014_01_22_171226_create_user_cloud_info_table.php
+++ app/database/migrations/2014_01_22_171226_create_user_cloud_info_table.php
@@ -9,15 +9,15 @@ class CreateUserCloudInfoTable extends Migration {
 	{
 		Schema::create('user_cloud_info',function($table) {
 			$table->increments('user_cloudID');
-			$table->string('email_cloud');// email id of that particular cloud
+			$table->string('user_cloud_name');// email id of that particular cloud
 			$table->integer('cloudID')->unsigned();
 			$table->foreign('cloudID')->references('cloudID')->on('clouds')->onDelete('cascade');
-			$table->unique(array('email_cloud,cloudID'));
 			
 
 			$table->integer('userID')->unsigned();
 			$table->foreign('userID')->references('userID')->on('users')->onDelete('cascade');
 			
+			$table->unique(array('userID','user_cloud_name','cloudID'));
 			$table->text('access_token');
 			$table->boolean('has_user_files');
 			//cursor :A string that encodes the latest information that has been returned. 
