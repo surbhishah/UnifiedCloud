aa72ac9c519f526ff6ef3e96326287088203ca6c
Surbhi <surbhishah81@yahoo.in>
2014-01-28 16:02:50 +0530

    Added SharedFile Model

---
+++ app/models/SharedFile.php
@@ -0,0 +1,11 @@
+<?php
+
+class SharedFile extends Eloquent  {
+
+	protected $table = 'shared_files';
+	protected $primaryKey = 'fileID';
+
+/**********************************************************************************************/	
+	
+	
+}
