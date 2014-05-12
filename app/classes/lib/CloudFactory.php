2c95f619302d16d0f00a74b6d059f2169ae09347
Surbhi <surbhishah81@yahoo.in>
2014-05-12 16:08:27 +0530

    CloudFactory now depends on reflection

--- app/classes/lib/CloudFactory.php
+++ app/classes/lib/CloudFactory.php
@@ -2,19 +2,25 @@
 class CloudFactory{
 	private $cloud;
 	public function createCloud($cloudName){
-		if(Str::lower($cloudName) == 'dropbox'){
-			$cloud = new Dropbox();
+		try{
+			$fileName = app_path()."/clouds.json";
+			$json_string = file_get_contents($fileName);
+			$json = json_decode($json_string);
+			$clouds = $json->clouds;
+			$clouds_lowerCase = array_map("strtolower", $clouds);
+			$index = array_search(strtolower($cloudName), $clouds_lowerCase);
+			if($index === FALSE)
+				throw new UnknownCloudException();
+			
+			$className = $clouds[$index];
+			$ref = new ReflectionClass($className);
+			$classInstance = $ref->newInstance();
+			return $classInstance;
+
+		}catch(ReflectionException $e){
+			Log::info("Exception raised in CloudFactory",array('CloudName'=>$cloudName));
+ 			Log::error($e);
+ 			throw $e;
 		}
-		else if(Str::lower($cloudName) == 'googledrive'){
-			$cloud = new GoogleDrive();
-		}
-		else if(Str::lower($cloudName)	=='skydrive'){
-			$cloud = new SkyDrive();
-		}
-		else{
-			$cloud = null;
-			throw new UnknownCloudException();
-		}
-		return $cloud;
 	}
 }
